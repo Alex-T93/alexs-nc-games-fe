cdba1ab023aaf3bd16c718b637d1ccd4d25d4dd9
Alex Tracey <alex.tracey@onthebeach.co.uk>
2022-09-27 14:04:51 +0100

    Reviews added

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+const Nav = (props) => {
+  const { setIsError, setError, setIsLoading } = props;
+
+  const handleError = () => {
+    setIsLoading(true);
+    setIsError(false);
+    setError(null);
+  };
+
+  return (
+    <section className='nav'>
+      <nav >
+   
+          <Link to='/api/'>Home</Link>
+          <Link to='/api/create_account' onClick={handleError}>
+            Create an account
+          </Link>
+          <Link to='/api/categories'>Categories</Link>
+          <Link to='/api/reviews'>Reviews</Link>
+          <Link to='/api/users'>Users</Link>
+      </nav>
+    </section>
+  );
+};
+
+export default Nav;
+
